43d40ab6550d56ea28c28097766011bbf491f1a2
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-02-10 20:27:04 +0200

    HW1 complted with optional task but without using Arrays class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,66 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+
+        for(int i=0; i<storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+
+        for(int i=0; i<storage.length; i++) {
+            if ( storage[i] != null && storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+
+        for(int i=0; i<storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                for (int j=i+1; j<storage.length && storage[j] != null; j++) {
+                    storage[j-1] = storage[j];
+                    storage[j] = null;
+                }
+
+                break;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] result = new Resume[size()];
+        for (int i=0; i<size(); i++){
+            result[i] = storage[i];
+        }
+
+        return result;
     }
 
     int size() {
-        return 0;
+        for(int i=0; i<storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
+
+        return storage.length;
     }
 }
