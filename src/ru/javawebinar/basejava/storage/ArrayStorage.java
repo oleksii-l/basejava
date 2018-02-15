e2f313d1e5e68e3ce32c9b07d3e9b14d26958e0c
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-02-15 18:31:31 +0200

    Template method pattern applied. SortedArrayStorage implemented. Added test for update() method

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -10,61 +10,22 @@ import java.util.Arrays;
 public class ArrayStorage extends AbstractArrayStorage {
 
 
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void save(Resume r) {
-
-        if (getIndexOf(r.getUuid()) != -1) {
-            System.out.println("ERROR: Resume is already present");
-            return;
-        }
-
-        if (size >= STORAGE_LIMIT) {
-            System.out.println("ERROR: storage is FULL");
-        }
-
+    @Override
+    protected void __save(int index, Resume r) {
         storage[size] = r;
-        size++;
     }
 
 
-    public void update(Resume r) {
-        int index = getIndexOf(r.getUuid());
-
-        if (index == -1) {
-            System.out.println("ERROR: Resume is NOT present");
-            return;
-        }
-
+    @Override
+    protected void __update(int index, Resume r) {
         storage[index] = r;
-
     }
 
 
-    public void delete(String uuid) {
-        int index = getIndexOf(uuid);
-
-        if (index == -1) {
-            System.out.println("ERROR: Resume is NOT present");
-            return;
-        }
-
+    @Override
+    protected void __delete(int index) {
         storage[index] = storage[size - 1];
         storage[size - 1] = null;
-        size--;
-
     }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-
-        return Arrays.copyOfRange(storage, 0, size);
-    }
-
 
 }
