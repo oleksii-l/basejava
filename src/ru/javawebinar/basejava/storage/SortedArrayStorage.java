8cde6e06ac424cd15ed4738da01c821784e642c2
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-02-15 22:17:05 +0200

    After code review:  - move getIndexOf from AbstractArrayStorage to ArrayStorage  - make update for SortedArrayStorage the same as for ArrayStorage  - remove copy of array from methods

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -8,29 +8,18 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
 
     @Override
-    protected void __update(int index, Resume r) {
-        __delete(index);
-        size--;
-        save(r);
-    }
-
-    @Override
     protected void __save(int index, Resume r) {
         int newIndex = -index - 1;
-        Resume[] destination = new Resume[STORAGE_LIMIT];
-        System.arraycopy(storage, 0, destination, 0, newIndex);
-        destination[newIndex] = r;
-        System.arraycopy(storage, newIndex, destination, newIndex + 1, size - newIndex);
+        System.arraycopy(storage, newIndex, storage, newIndex + 1, size - newIndex);
+        storage[newIndex] = r;
+        System.arraycopy(storage, 0, storage, 0, newIndex);
 
-        storage = destination;
     }
 
     @Override
     protected void __delete(int index) {
-        Resume[] destination = new Resume[STORAGE_LIMIT];
-        System.arraycopy(storage, 0, destination, 0, index);
-        System.arraycopy(storage, index + 1, destination, index, size - index);
-        storage = destination;
+        System.arraycopy(storage, 0, storage, 0, index);
+        System.arraycopy(storage, index + 1, storage, index, size - index);
     }
 
 
