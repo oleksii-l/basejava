8cba656cfcaca6359f6ed07b42997c0093d681c9
Oleksii Leshchenko <oleksii.leshchenko@gmail.com>
2018-03-26 14:35:23 +0300

    1. Take into account that there can be different periods for the same organisation

--- src/ru/javawebinar/basejava/model/Organization.java
+++ src/ru/javawebinar/basejava/model/Organization.java
@@ -1,6 +1,11 @@
 package ru.javawebinar.basejava.model;
 
+import javafx.util.Pair;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -10,8 +15,7 @@ import java.util.Objects;
 public class Organization {
     private final Link homePage;
 
-    private final LocalDate startDate;
-    private final LocalDate endDate;
+    private List<Pair<LocalDate, LocalDate>> periods = new ArrayList<>();
     private final String title;
     private final String description;
 
@@ -20,43 +24,38 @@ public class Organization {
         Objects.requireNonNull(endDate, "endDate must not be null");
         Objects.requireNonNull(title, "title must not be null");
         this.homePage = new Link(name, url);
-        this.startDate = startDate;
-        this.endDate = endDate;
         this.title = title;
         this.description = description;
+        addPeriod(startDate, endDate);
+    }
+
+    public void addPeriod(LocalDate startDate, LocalDate endDate) {
+        Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(endDate, "endDate must not be null");
+        periods.add(new Pair<>(startDate, endDate));
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Organization that = (Organization) o;
-
-        if (!homePage.equals(that.homePage)) return false;
-        if (!startDate.equals(that.startDate)) return false;
-        if (!endDate.equals(that.endDate)) return false;
-        if (!title.equals(that.title)) return false;
-        return description != null ? description.equals(that.description) : that.description == null;
-
+        return Objects.equals(homePage, that.homePage) &&
+                Objects.equals(periods, that.periods) &&
+                Objects.equals(title, that.title);
     }
 
     @Override
     public int hashCode() {
-        int result = homePage.hashCode();
-        result = 31 * result + startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        result = 31 * result + title.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        return result;
+
+        return Objects.hash(homePage, periods, title);
     }
 
     @Override
     public String toString() {
         return "Organization{" +
                 "homePage=" + homePage +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
+                ", periods=" + periods +
                 ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
                 '}';
